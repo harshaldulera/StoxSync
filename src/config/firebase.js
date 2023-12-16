6f1d57e16b04fab38e2015d981a032cc80e8a4e0
Harshal Dulera <harshaldulera82@gmail.com>
2023-12-16 15:53:10 +0530

    phno

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -8,18 +8,18 @@ import { getAuth } from "firebase/auth";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: import.meta.VITE_APIKEY,
-    authDomain: import.meta.VITE_AUTH_DOMAIN,
-    projectId: import.meta.VITE_PROJECT_ID,
-    storageBucket: import.meta.VITE_STORAGE_BUCKET,
-    messagingSenderId: import.meta.VITE_MESSAGING_ID,
-    appId: import.meta.VITE_APP_ID,
-    measurementId: import.meta.VITE_MEASUREMENT_ID
+    apiKey: import.meta.env.VITE_APIKEY,
+    authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+    projectId: import.meta.env.VITE_PROJECT_ID,
+    storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+    messagingSenderId: import.meta.env.VITE_MESSAGING_ID,
+    appId: import.meta.env.VITE_APP_ID,
+    measurementId: import.meta.env.VITE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const auth = getAuth(firebaseApp);
+const auth = getAuth(app);
 const analytics = getAnalytics(app);
 
 export { auth };
