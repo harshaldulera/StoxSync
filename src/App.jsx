389d1c945ec77b6abc807911672e0eef296280cb
Harshal Dulera <harshaldulera82@gmail.com>
2023-12-16 14:34:26 +0530

    config

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import {
-  BrowserRouter,
+  BrowserRouter as Router,
   Route,
   Routes,
 } from "react-router-dom";
 import "./App.css";
+
+import Login from "./pages/Login";
 
 function App () {
   return (
@@ -12,7 +14,7 @@ function App () {
       <div className="root">
         <Router>
           <Routes>
-            <Route path="/" element={} />
+            <Route path="/login" element={<Login />} />
           </Routes>
         </Router>
       </div>
